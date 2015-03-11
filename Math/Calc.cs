0660e73c0ebf0a0e1003cd20287e83e367d57af1
Sebastian Esp <private@sebastianesp.dk>
2015-03-11 14:03:57 +0100

    Added project to Source Control

---
+++ Math/Calc.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Math
+{
+    class Calc
+    {
+        public string[] function(string func)
+        {
+            Regex regex = new Regex(@"\w\(([\w]*)\)=([\d]*|[\w]*)(\**)((\1))(\+)([\w]*|[\d]*)");
+            Match match = regex.Match(func);
+
+            string[] result = new string[match.Groups.Count];
+
+            for (int i = 0; i < match.Groups.Count; i++)
+            {
+                result[i] = match.Groups[i].ToString();
+            }
+            return result;
+        }
+
+        public string getResult(string func)
+        {
+            Regex regex = new Regex(@"\w+\((\w+)\)=");
+            Match match = regex.Match(func);
+
+            string result = match.Groups[1].ToString();
+            return result;
+        }
+
+        public string[] getPoints(string points)
+        {
+            Regex regex = new Regex(@"\((\-*\d+|\w+)\.(\-*\d+|\w+)\)");
+            Match match = regex.Match(points);
+
+            string[] result = new string[match.Groups.Count];
+
+            for (int i = 0; i < match.Groups.Count; i++)
+            {
+                result[i] = match.Groups[i].ToString();
+            }
+            return result;
+        }
+        public double Y(double a, double x, double b)
+        {
+            double y = a*x+b;
+            return y;
+        }
+
+        public double A(double y1, double y2, double x1, double x2)
+        {
+                double a = (y2 - y1) / (x2 - x1);
+                return a;
+        }
+
+        public double B(double y1, double a, double x1)
+        {
+            double b = y1 - a * x1;
+            return b;
+        }
+    }
+}
---
+++ Math/MainWindow.xaml.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Math
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Calc calc = new Calc();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        #region Function
+        private void txtBox_function_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void btn_Calc_Click(object sender, RoutedEventArgs e)
+        {
+            string[] function = calc.function(txtBox_function.Text);
+            txtBox_a.Text = function[2];
+            txtBox_b.Text = function[7];
+        }
+        #endregion
+
+        #region Points1
+        private void txtBox_Points1_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+        private void btn_CalcPoints2_Click(object sender, RoutedEventArgs e)
+        {
+            string[] points = calc.getPoints(txtBox_Points1.Text);
+            txtBox_x1.Text = points[1];
+            txtBox_y1.Text = points[2];
+        }
+        #endregion
+
+        #region Points2
+        private void txtBox_Points2_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void btn_CalcPoints1_Click(object sender, RoutedEventArgs e)
+        {
+            string[] points = calc.getPoints(txtBox_Points2.Text);
+            txtBox_x2.Text = points[1];
+            txtBox_y2.Text = points[2];
+        }
+        #endregion
+
+        #region Calc A
+        private void txtBox_a_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void btn_a_Click(object sender, RoutedEventArgs e)
+        {
+            string result = calc.A(Double.Parse(txtBox_y2.Text), Double.Parse(txtBox_y1.Text), Double.Parse(txtBox_x2.Text), Double.Parse(txtBox_x1.Text)).ToString();
+            txtBox_a.Text = result;
+        }
+
+        #endregion 
+
+        #region Calc B
+        private void txtBox_b_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+        private void btn_b_Click(object sender, RoutedEventArgs e)
+        {
+            string result = calc.B(Double.Parse(txtBox_y1.Text), Double.Parse(txtBox_a.Text), Double.Parse(txtBox_x1.Text)).ToString();
+            txtBox_b.Text = result;
+        }
+        #endregion
+
+        #region Calc Fx
+        private void txtBox_Result_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void btn_CalcFx_Click(object sender, RoutedEventArgs e)
+        {
+            string function = calc.getResult(txtBox_Result.Text);
+            txtBox_Result.Text = "f(" + function + ")=" + calc.Y(Double.Parse(txtBox_a.Text), Double.Parse(function), Double.Parse(txtBox_b.Text)).ToString();
+        }
+        #endregion
+    }
+}
